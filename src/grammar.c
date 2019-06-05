1d25ae1d2fbe7eb57f920240091cb9d73bac4fe3
autocommit <autocommit>
2019-06-05 18:17:01 +0000

    auto 0 146 1784

---
+++ src/grammar.c
@@ -0,0 +1,91 @@
+#include "mystring.h"
+
+void parse(string buf) {
+	size_t pos = 0;
+	bool advancef(string s) {
+		size_t left = buf.len - pos;
+		if(left < s.len) return false;
+		if(0 == memcmp(s.base, buf.base + pos, s.len)) return true;
+		return false;
+	}
+	
+	void eat_space(void) {
+		while(isspace(buf.base[pos])) {
+			if(++pos == buf.len) longjmp(&onerr, done);
+		}
+		if(advance("/*")) {
+			eat_space();
+			if(pos
+	}
+
+	auto void for_statement(void);
+	
+#define advance(lit) advancef(LITSTR(s));
+	int done = -1;
+	int err = setjmp(&onerr);
+	if(err == 0) {
+		while(pos < buf.len) {
+			if(advance("RETURNS")) {
+				output_return_type();
+			} else if(advance("CLOSURE")) {
+				output_closure_name();
+			} else if(for_statement()) {
+			} else {
+				fputc(buf.base[pos], ' ');
+				++pos;
+			}
+		}
+	} else if(err == 1) {
+		// ok
+	} else {
+		fprintf(stderr, "Uhh %d\n", err);
+		abort();
+	}
+	bool for_statement(void) {
+		eat_space();
+		if(pos == buf.len) return false;
+		if(!advance("FOR_STATEMENT")) return false;
+		eat_space();
+		bool do_init = false;
+		if(advance("INIT")) {
+			do_init = true;
+			eat_space();
+		}
+		bool do_type = false;
+		bool do_name = false;
+		if(advance("type")) {
+			eat_space();
+			do_type = true;
+			if(pos == buf.len) {
+				output_for_statement(do_init, do_type, false, LITSTR(""));
+				longjmp(&onerr, 2);
+			}
+		}
+		if(advance("name")) {
+			eat_space();
+			if(pos == buf.len) {
+				output_for_statement(do_init, do_type, true, LITSTR(""));
+				longjmp(&onerr, 3);
+			}
+		}
+		string delim = {
+			buf.base + pos,
+			1;
+		};
+		while(!isspace(buf.base[++pos])) {
+			++delim.len;
+			if(pos == buf.len) {
+				output_for_statement(do_init, do_type, do_name, delim);
+				longjmp(&onerr, 1);
+			}
+		}
+		while(!advance("END_FOR_STATEMENT")) {
+			if(++pos == buf.len) {
+				longjmp(&onerr, 3);
+			}
+		}
+			
+		output_for_statement(do_init, do_type, do_name, delim);
+		return true;
+	}
+}
