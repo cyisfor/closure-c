130bab81349ee778837a08e59f184e8f5163db71
autocommit <autocommit>
2019-06-05 19:47:39 +0000

    auto 0 101 885

--- src/closure.interface.h
+++ src/closure.interface.h
@@ -12,43 +12,17 @@ ignore leading space, FOR_TYPES, ignore whitespace, start here, end is at END_FO
 everything between last `name` and END_FOR_TYPES
  */
 
-struct CLOSURE {
-	void* arg;
-	FOR_TYPES INIT
-	type name;
-	END_FOR_TYPES
-	FOR_TYPES
-	type name;
-	END_FOR_TYPES;
-	CLOSURE_call call;
-};
+struct CLOSURE;
 
-static struct CLOSURE CLOSURE(
+struct CLOSURE CLOSURE(
 	CLOSURE_call call,
 	void* arg,
 	FOR_TYPES INIT
 	type name,
-	END_FOR_TYPES) {
-	return (struct CLOSURE) {
-		.call = call,
-		.arg = arg,
-		FOR_TYPES INIT
-		.name = name,
-		END_FOR_TYPES
-	};
-}
+	END_FOR_TYPES);
 
-static struct RETURNS call_CLOSURE(
+struct RETURNS call_CLOSURE(
 	const CLOSURE* self,
 	FOR_TYPES
 	type name,
-	END_FOR_TYPES) {
-	return self->call(
-		self->arg,
-		FOR_TYPES INIT
-		self->name,
-		END_FOR_TYPES
-		FOR_TYPES
-		name,
-		END_FOR_TYPES);
-}
+	END_FOR_TYPES);
