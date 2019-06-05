c59dc42305daa2260db750c4113993322f723cdf
autocommit <autocommit>
2019-06-05 20:26:57 +0000

    auto 0 45 420

--- src/example_main.c
+++ src/example_main.c
@@ -1,8 +1,9 @@
 #include "example_closure.h"
 
-struct thing my_callback(struct thing thing, void* arg, char foo, string bar) {
+struct thing my_callback(void* arg, struct thing thing, void* arglebargle,
+						 char foo, string bar) {
 	printf("thing: %d\n", thing.foo);
-	printf("arg: %p\n", arg);
+	printf("arg: %p %p\n", arg, arglebargle);
 	printf("foo: %c\n", foo);
 	printf("bar: %.*s\n", (int)bar.len, bar.base);
 	thing.foo += 23;
@@ -14,9 +15,9 @@ int main(int argc, char *argv[])
 	struct thing thing = {
 		.foo = 19;
 	};
-    struct example_closure c = example_closure(my_callback, {
+    struct example_closure c = example_closure(my_callback, (void*)0x42, {
 			.oof = thing,
-				.arg = NULL
+				.arglebargle = (void*)0x23
 				});
 	struct thing answer = example_closure_call(c, 'a', LITSTR("bee"));
 	printf("answer %d\n", answer.foo);
