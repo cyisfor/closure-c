40d2680ecef3933e31cab37695dbd3f87894404a
autocommit <autocommit>
2019-06-05 22:34:46 +0000

    auto 0 30 480

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
+#include "serialized_interface.h"
 #include "mmapfile.h"
 #include "grammar.h"
 
 int main(int argc, char *argv[])
 {
-	size_t size;
-	char* mem = mmapfd(0, &size);
-	if(mem == NULL) return 1;
-	parse((string){mem, size});
+	parse((string){INTERFACE, INTERFACE_SIZE});
     return 0;
 }
