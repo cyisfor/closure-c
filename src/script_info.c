8bb7ba45c72ef8b6dc909392b1e182f5a132e73e
autocommit <autocommit>
2019-06-07 01:59:27 +0000

    auto 0 141 1282

--- src/script_info.c
+++ src/script_info.c
@@ -11,12 +11,6 @@
 #include <stdarg.h> // va_*
 #include <signal.h> // raise
 
-
-
-static void showbuf(string s, size_t pos) {
-	printf("BUF: =====\n%.*s\n====\n", (int)(s.len-pos), s.base+pos);
-}
-
 struct var* aux_vars = NULL;
 struct var* vars = NULL;
 size_t nvars = 0;
@@ -26,6 +20,144 @@ bstring preamble = {};
 
 string closure_name = {};
 string return_type = {};
+
+enum failure_state {
+	SUCCESS,
+	NO_SPACES_FOUND,
+	NO_NAME,
+	NO_RETURN,
+	EXTRANEOUS
+};
+
+
+struct parser {
+	string buf;
+	size_t pos;
+	jmp_buf onerr;
+	bool noexit;
+#ifdef OUTPUT
+	bool output;
+#endif
+};	
+
+#include "parser-snippet.h"
+
+static
+void showbuf(string s, size_t pos) {
+	printf("BUF: =====\n%.*s\n====\n", (int)(s.len-pos), s.base+pos);
+}
+
+static
+bool consume_universal_stuff(struct parser* p) {
+	return false;
+}
+
+static
+bool consume_statement(struct parser* p, string* line) {
+	size_t start = P(pos);
+	if(seek(";")) {
+		line->base = P(P(buf.base)) + start;
+		line->len = P(pos) - start;
+		++P(pos); // consume(";");
+	} else {
+		line->base = P(P(buf.base)) + start;
+		line->len = buf.len - start;
+		P(pos) = buf.len;
+	}
+	return true;
+}
+
+
+
+	/* smallest var:
+	   start_type = 0
+	   end_type = 1
+	   (1 space)
+	   start_name = 3
+	   end_name = 4
+	*/
+static
+bool consume_var(struct parser* p) {
+	string line;
+	size_t oldpos = P(pos);
+	consume_statement(p, &line);
+	size_t start_type = 0;
+	size_t end_name = line.len;
+	if(end_name < 4) {
+		P(pos) = oldpos;
+		return false;
+	}
+	/* have to work backwards because no space in identifiers */
+	if(end_name <= start_type) {
+		P(pos) = oldpos;
+		return false;
+	}
+	size_t start_name = end_name-1;
+	for(;;) {
+		if(start_name < 3) {
+			P(pos) = oldpos;
+			return false;
+		}
+		if(isspace(line.base[start_name-1])) break;
+		--start_name;
+	}
+	size_t end_type = start_name ;
+	for(;;) {
+		if(--end_type == start_type) {
+			fail(NO_SPACES_FOUND,
+				 "no space in argument definition");
+		}
+		if(!isspace(line.base[end_type-1])) break;
+	}
+	struct var v = {
+		.type = (string){
+			.base = line.base + start_type,
+			.len = end_type - start_type
+		},
+		.name = (string) {
+			.base = line.base + start_name,
+			.len = end_name - start_name
+		}
+	};
+	if(aux) {
+		++naux_vars;
+		aux_vars = reallocarray(aux_vars,
+								naux_vars,
+								sizeof(*aux_vars));
+		assert(aux_vars);
+		aux_vars[naux_vars-1] = v;
+	} else {
+		++nvars;
+		vars = reallocarray(vars,
+							nvars,
+							sizeof(*vars));
+		assert(vars);
+		vars[nvars-1] = v;
+	}
+	return true;
+}
+
+static
+bool consume_include(struct parser* p) {
+	size_t start = P(pos);
+	eat_space(p);
+	if(consume(p, "#include ")) {
+		eat_space(p);
+		size_t start2 = P(pos);
+		if(seek("\n")) {
+			straddn(&preamble, LITLEN("#include "));
+			straddn(&preamble,
+					P(buf.base) + start2,
+					P(pos) - start2 + 1);
+			return true;
+		} else {
+			P(pos) = start;
+			return false;
+		}
+	}
+	P(pos) = start;
+	return false;
+}
 
 void script_info_load(int fd) {
 	size_t size = 0;
@@ -44,129 +176,15 @@ void script_info_load(int fd) {
 		}
 	}
 			
-	string buf = {script, size};
-
-	enum failure_state {
-		SUCCESS,
-		NO_SPACES_FOUND,
-		NO_NAME,
-		NO_RETURN,
-		EXTRANEOUS
-	};
+	struct parser pp = {
+		.buf = {script, size},
+		.aux = false
+	}
 	
-#include "parser-snippet.h"
-
-	bool consume_universal_stuff(void) {
-		return false;
-	}
-	bool consume_statement(string* line) {
-		size_t start = pos;
-		if(seek(";")) {
-			line->base = buf.base + start;
-			line->len = pos - start;
-			++pos; // consume(";");
-		} else {
-			line->base = buf.base + start;
-			line->len = buf.len - start;
-			pos = buf.len;
-		}
-		return true;
-	}
-
-	bool aux = false;
-
-	/* smallest var:
-	   start_type = 0
-	   end_type = 1
-	   (1 space)
-	   start_name = 3
-	   end_name = 4
-	*/
-
-	bool consume_var(void) {
-		string line;
-		size_t oldpos = pos;
-		consume_statement(&line);
-		size_t start_type = 0;
-		size_t end_name = line.len;
-		if(end_name < 4) {
-			pos = oldpos;
-			return false;
-		}
-		/* have to work backwards because no space in identifiers */
-		if(end_name <= start_type) {
-			pos = oldpos;
-			return false;
-		}
-		size_t start_name = end_name-1;
-		for(;;) {
-			if(start_name < 3) {
-				pos = oldpos;
-				return false;
-			}
-			if(isspace(line.base[start_name-1])) break;
-			--start_name;
-		}
-		size_t end_type = start_name ;
-		for(;;) {
-			if(--end_type == start_type) {
-				fail(NO_SPACES_FOUND,
-					 "no space in argument definition");
-			}
-			if(!isspace(line.base[end_type-1])) break;
-		}
-		struct var v = {
-			.type = (string){
-				.base = line.base + start_type,
-				.len = end_type - start_type
-			},
-			.name = (string) {
-				.base = line.base + start_name,
-				.len = end_name - start_name
-			}
-		};
-		if(aux) {
-			++naux_vars;
-			aux_vars = reallocarray(aux_vars,
-									 naux_vars,
-									 sizeof(*aux_vars));
-			assert(aux_vars);
-			aux_vars[naux_vars-1] = v;
-		} else {
-			++nvars;
-			vars = reallocarray(vars,
-								nvars,
-								sizeof(*vars));
-			assert(vars);
-			vars[nvars-1] = v;
-		}
-		return true;
-	}
-
-	bool consume_include() {
-		size_t start = pos;
-		eat_space();
-		if(consume("#include ")) {
-			eat_space();
-			size_t start2 = pos;
-			if(seek("\n")) {
-				straddn(&preamble, LITLEN("#include "));
-				straddn(&preamble,
-						buf.base + start2,
-						pos - start2 + 1);
-				return true;
-			} else {
-				pos = start;
-				return false;
-			}
-		}
-		pos = start;
-		return false;
-	}	
 	
-	int err = setjmp(onerr);
+		int err = setjmp(onerr);
 	if(err == 0) {
-		size_t start = pos;
+		size_t start = P(pos);
 		while(consume_include());
 		eat_space();
 		// #include etc\n always adds an extra \n at the end
@@ -194,7 +212,7 @@ void script_info_load(int fd) {
 				aux = true;
 			} else if(consume_var()) {
 				// ok
-			} else if(buf.len == pos) {
+			} else if(buf.len == P(pos)) {
 				break;
 			} else {
 				fail(EXTRANEOUS, "extraneous characters");
@@ -205,7 +223,7 @@ void script_info_load(int fd) {
 	} else {
 		fprintf(stderr, "uhhh %d\n", err);
 		fprintf(stderr, "what '%.*s'\n",
-				(int)(buf.len - pos), buf.base+pos);
+				(int)(buf.len - P(pos)), P(P(buf.base))+P(pos));
 		abort();
 	}
 }
